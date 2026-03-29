37278d5f818e8a7070b246fe70ba0544cb7f9494
rafialtafrivara <rafialtafrivara@gmail.com>
2026-03-30 00:51:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Aspirasi Siswa</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+            <h1>Aspirasi Siswa</h1>
+            <ul>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="aspirasi-siswa.php">Aspirasi</a></li>
+                <li><a href="login.php">Login Admin</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- HERO -->
+    <div class="hero">
+        <h1>Suaramu Sangat Berarti 🎯</h1>
+        <p>
+            Website ini dibuat untuk menampung aspirasi, keluhan, dan saran dari siswa
+            demi menciptakan lingkungan sekolah yang lebih baik, nyaman, dan berkualitas.
+            Setiap laporan yang kamu kirim akan diproses dan ditindaklanjuti oleh pihak sekolah.
+        </p>
+
+        <a href="aspirasi-siswa.php" class="btn-hero">🚀 Ayo Berikan Aspirasi</a>
+    </div>
+
+    <!-- FITUR -->
+    <div class="section fitur">
+        <div class="container">
+            <h2 style="text-align:center; margin-bottom:20px;">Kenapa Harus Menggunakan Sistem Ini?</h2>
+
+            <div class="fitur-container">
+
+                <div class="fitur-box">
+                    <h3>📢 Mudah Digunakan</h3>
+                    <p>
+                        Kirim aspirasi hanya dalam beberapa langkah tanpa ribet.
+                    </p>
+                </div>
+
+                <div class="fitur-box">
+                    <h3>⚡ Proses Cepat</h3>
+                    <p>
+                        Setiap laporan akan langsung diterima dan diproses oleh admin.
+                    </p>
+                </div>
+
+                <div class="fitur-box">
+                    <h3>📊 Transparan</h3>
+                    <p>
+                        Kamu bisa melihat status laporan apakah sedang diproses atau sudah selesai.
+                    </p>
+                </div>
+
+                <div class="fitur-box">
+                    <h3>💬 Ada Feedback</h3>
+                    <p>
+                        Setiap aspirasi akan mendapatkan tanggapan dari pihak sekolah.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <footer>
+        <small>© 2026 - Sistem Aspirasi Siswa</small>
+    </footer>
+
+</body>
+
+</html>
